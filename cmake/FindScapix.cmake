6199467d0fe035bd5a7c9b6a862aa0dbcc6a28dc
Fabrizio Destro <7031675+dexpota@users.noreply.github.com>
2021-09-27 15:31:23 +0200

    Update FindScapix.cmake

--- cmake/FindScapix.cmake
+++ cmake/FindScapix.cmake
@@ -1,7 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/cmodule.cmake)
 
 cmodule_add(
-  scapix 1.0.25
-  URL "https://github.com/scapix-com/scapix/archive/v1.0.25.tar.gz"
-  URL_HASH SHA256=a42cd50367ec9e61c47da12f71bd83d336c1c6fd6631c22af96faa213679585f
+  scapix 1.0.26
+  URL "https://github.com/dexpota/scapix/archive/refs/tags/1.0.26-dexpota.tar.gz"
+  URL_HASH SHA256=9267b044f8ac3f4c1ab18a0e5867502cb321d16cdbf9f20d154781c190d2aeb5
 )
